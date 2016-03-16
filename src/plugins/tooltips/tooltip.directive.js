5a1e7dcdcc9c8b633a94c78088e4077153bd35e9
Cyril Sobierajewicz <sobierajewicz.cyril@gmail.com>
2016-03-16 15:52:45 +0100

    fix(sticky-tooltips): close the previous sticked tooltip on open

--- src/plugins/tooltips/tooltip.directive.js
+++ src/plugins/tooltips/tooltip.directive.js
@@ -121,8 +121,27 @@
                 });
 
                 $scope.task.getContentElement().bind('mouseenter', function(evt) {
+                    var taskTooltips = $scope.task.model.tooltips;
+                    var rowTooltips = $scope.task.row.model.tooltips;
+
+                    if (typeof(taskTooltips) === 'boolean') {
+                        taskTooltips = {enabled: taskTooltips};
+                    }
+
+                    if (typeof(rowTooltips) === 'boolean') {
+                        rowTooltips = {enabled: rowTooltips};
+                    }
+
+                    var sticky = utils.firstProperty([taskTooltips, rowTooltips], 'sticky', $scope.pluginScope.sticky);
+
                     mouseEnterX = evt.clientX;
                     displayTooltip(true, true);
+
+                    if (sticky) {
+                        $scope.gantt.api.tooltips.stick(function () {
+                            displayTooltip(false, false);
+                        });
+                    }
                 });
 
                 $scope.task.getContentElement().bind('mouseleave', function() {
